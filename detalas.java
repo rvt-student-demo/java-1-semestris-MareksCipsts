5bdb7a5b9b95a8f69a08bda2ca60bac999f07d08
MareksCipsts <a240922mc@rvt.lv>
2025-10-09 14:41:24 +0300

    detalas java

---
+++ detalas.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class detalas {
+    public static void main (String[] args) {
+        System.out.print("Bob's Discount Bolts");
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ievadi skrūvju skaitu:");
+        int skruves = sc.nextInt();
+        int skrcena = skruves * 5;
+        System.out.print("Ievadi uzriežņu skaitu:");
+        int uzgr = sc.nextInt();
+        int uzgrcena = uzgr * 3;
+        System.out.print("Ievadi paplākšņu skaitu:");
+        int papl = sc.nextInt();
+        int paplcena = papl * 1;
+        int total = skrcena + uzgrcena + paplcena;
+        if (skruves > uzgr) {
+            System.out.println("Pārbaudi pasūtījumu: par maz uzgriežņu");
+        }
+        if (skruves * 2 > papl) {
+            System.out.println("Pārbaudi pasūtījumu: par maz paplāksņu");
+        } 
+        if (uzgr >= skruves && papl >= skruves * 2) {
+            System.out.println("Pasūtījums ir pareizs");
+        }
+        
+        System.out.println("Kopējā cena:" + total);
+    }
+}
